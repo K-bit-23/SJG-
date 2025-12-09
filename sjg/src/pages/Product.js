3d6bbdae2f094ddf0609cecf35b965935a963bcc
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-12-09 15:47:05 +0000

    n

--- sjg/src/pages/Product.js
+++ sjg/src/pages/Product.js
@@ -7,7 +7,7 @@ const Product = ({ product, addToCart }) => {
       <img src={product.image} alt={product.name} />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
-      <button onClick={() => addToCart(product)}>Add to Cart</button>
+      <button className="add-to-cart" onClick={() => addToCart(product)}>Add to Cart</button>
     </div>
   );
 };
