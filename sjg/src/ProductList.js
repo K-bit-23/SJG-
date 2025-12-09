a07d2f5a294dab62bc3538be7c86765b251920f0
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-12-09 15:29:08 +0000

    product

--- sjg/src/ProductList.js
+++ sjg/src/ProductList.js
@@ -4,12 +4,13 @@ import Product from './Product';
 
 const ProductList = ({ products, addToCart }) => {
   return (
-    <div className="product-list">
-      {products.map(product => (
-        <Product key={product.id} product={product} addToCart={addToCart} />
-      ))}
-    </div>
-  );
-};
+      <div className="product-list">
+            {products.map(product => (
+                    <Product key={product.id} product={product} addToCart={addToCart} />
+                          ))}
+                              </div>
+                                );
+                                };
 
-export default ProductList;
+                                export default ProductList;
+                                
