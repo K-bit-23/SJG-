4d6e38af1680af896a8bb4913f9058a8fd9dbe21
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-12-31 07:58:47 +0000

    n

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Services from './pages/Services';
-import About from './pages/About';
 import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Orders from './pages/Orders';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import './App.css';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products/')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (productToRemove) => {
+    setCart(cart.filter(product => product !== productToRemove));
+  };
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <Navbar cartCount={cart.length} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/products" element={<Products products={products} addToCart={addToCart} />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
         <Footer />
       </div>
