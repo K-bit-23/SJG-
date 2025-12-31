ecd152d0ba8fc0cdee92a2a3192d6c587850149f
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-12-31 08:14:55 +0000

    orders

---
+++ frontend/src/pages/Orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div className="orders-container">
+      <h1>Your Orders</h1>
+      <p>You have no recent orders.</p>
+      {/* Add more detailed order information here */}
+    </div>
+  );
+};
+
+export default Orders;
