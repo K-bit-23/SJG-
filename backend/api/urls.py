151d17c29ff3c82c878e5546c0155368df17710f
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-12-31 08:06:10 +0000

    Updated backend for user authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
