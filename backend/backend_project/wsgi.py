fee1a33e6c7cec39b84067a39b0f2cec78e5c4b9
karthikeyan_p <dkarthideepak@gmail.com>
2026-03-18 11:11:52 +0530

    Fix Render email networking issue by forcing IPv4 sockets

--- backend/backend_project/wsgi.py
+++ backend/backend_project/wsgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import socket
+
+# Force IPv4 universally specifically for Render/Free-tiers which drop IPv6 SMTP
+old_getaddrinfo = socket.getaddrinfo
+def new_getaddrinfo(*args, **kwargs):
+    responses = old_getaddrinfo(*args, **kwargs)
+    # Filter out IPv6 results (AF_INET6)
+    return [response for response in responses if response[0] == socket.AF_INET]
+socket.getaddrinfo = new_getaddrinfo
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_project.settings')
 
 application = get_wsgi_application()
